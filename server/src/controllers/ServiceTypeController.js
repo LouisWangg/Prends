7f81f26e6eec4855caf2afc21f50c5d2f9729504
LouisWangg <louisfernandoyantowang@gmail.com>
2025-05-20 22:24:37 +0700

    Install multer in backend

--- server/src/controllers/ServiceTypeController.js
+++ server/src/controllers/ServiceTypeController.js
@@ -1,4 +1,6 @@
 const { Op } = require("sequelize");
+const multer = require('multer');
+const upload = multer();  // in-memory storage
 const serviceTypeModel = require("../models/ServiceTypeModel");
 
 // Get Konseling Individu datas 
@@ -27,6 +29,29 @@ const getIndividualCounseling = async (req, res) => {
         attributes: ['itemType', 'name', 'price', 'discountFlag', 'discountPrice', 'duration', 'description']
     });
     res.json(data);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+};
+
+// Upload image by id
+const uploadImage = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!req.file) return res.status(400).send("No image file uploaded.");
+
+    // Find user by primary key
+    const data = await serviceTypeModel.findByPk(id);
+    if (!data) return res.status(404).send("User not found");
+
+    // Save image buffer (bytea) to DB
+    await serviceTypeModel.update(
+      { image: req.file.buffer },
+      { where: { serviceTypeId: id } }
+    );
+
+    res.send("User image uploaded successfully");
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Server error");
